856880c63d3c6bf467fbf0d12ab0c38256ddc33c
marouane101 <marouaneelaissi2@gmail.com>
2024-08-07 16:06:10 +0100

    Added UserPlant Logic

---
+++ Controllers/UserPlantController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TE_trsprt_remake.DTOs;
+using TE_trsprt_remake.Models;
+using TE_trsprt_remake.Services;
+
+namespace TE_trsprt_remake.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserPlantController : ControllerBase
+    {
+        private readonly IUserPlantService _userPlantService;
+
+        public UserPlantController(IUserPlantService userPlantService)
+        {
+            _userPlantService = userPlantService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserPlant>>> GetUserPlants()
+        {
+            return await _userPlantService.GetUserPlants();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddUserPlant(UserPlantDTO userPlantDTO)
+        {
+            var result = await _userPlantService.AddUserPlant(userPlantDTO);
+            if (result)
+            {
+                return Ok("UserPlant created successfully.");
+            }
+            return BadRequest("Error creating UserPlant.");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUserPlant(int id, UserPlantDTO userPlantDTO)
+        {
+            var result = await _userPlantService.UpdateUserPlant(userPlantDTO, id);
+            if (result)
+            {
+                return Ok("UserPlant updated successfully.");
+            }
+            return BadRequest("Error updating UserPlant.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserPlant(int id)
+        {
+            var result = await _userPlantService.DeleteUserPlant(id);
+            if (result)
+            {
+                return Ok("UserPlant deleted successfully.");
+            }
+            return BadRequest("Error deleting UserPlant.");
+        }
+    }
+}
---
+++ Models/UserPlantService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TE_trsprt_remake.Data;
+using TE_trsprt_remake.DTOs;
+using TE_trsprt_remake.Models;
+
+namespace TE_trsprt_remake.Services
+{
+    public class UserPlantService : IUserPlantService
+    {
+        private readonly AppDbContext _context;
+
+        public UserPlantService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<UserPlant>>> GetUserPlants()
+        {
+            return await _context.UserPlants.ToListAsync();
+        }
+
+        public async Task<bool> AddUserPlant(UserPlantDTO userPlant)
+        {
+            var up = new UserPlant
+            {
+                UserId = userPlant.UserId,
+                PlantId = userPlant.PlantId
+            };
+
+            _context.UserPlants.Add(up);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteUserPlant(int id)
+        {
+            var up = await _context.UserPlants.FirstOrDefaultAsync(up => up.Id == id);
+            if (up == null)
+            {
+                return false;
+            }
+
+            _context.UserPlants.Remove(up);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateUserPlant(UserPlantDTO userPlant, int id)
+        {
+            var existingUserPlant = await _context.UserPlants.FirstOrDefaultAsync(up => up.Id == id);
+            if (existingUserPlant == null)
+            {
+                return false;
+            }
+
+            existingUserPlant.UserId = userPlant.UserId;
+            existingUserPlant.PlantId = userPlant.PlantId;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
